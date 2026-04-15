429fd71ba3a084363e22419389fdc02e0aacd5fa
RyRy-Winner <ryker.ballash@gmail.com>
2026-04-15 10:00:25 -0600

    Board startup

--- board.cpp
+++ board.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "board.h"
+
+board::board()
+{
+
+}
+
+void board::resetBoard()
+{
+    //
+    lastMove =
+
+    whitePawns = 0x000000000000ff00ULL;
+    whiteRooks = 0x0000000000000081ULL;
+    whiteKnights = 0x0000000000000042ULL;
+    whiteBishops = 0x0000000000000024ULL;
+    whiteQueens = 0x0000000000000008ULL;
+    whiteKings = 0x0000000000000010ULL;
+
+    blackPawns = 0x00ff000000000000ULL;
+    blackRooks = 0x8100000000000000ULL;
+    blackKnights = 0x4200000000000000ULL;
+    blackBishops = 0x2400000000000000ULL;
+    blackQueens = 0x0800000000000000ULL;
+    blackKings = 0x1000000000000000ULL;
+
+
+}
--- board.h
+++ board.h
@@ -1,6 +1,7 @@
 //
 // Created by rbal7885 on 4/15/2026.
 //
+#include <stdint.h>;
 
 #ifndef CENGINE_BOARD_H
 #define CENGINE_BOARD_H
@@ -9,6 +10,17 @@
 
 class board {
 public:
+    board();
+    void resetBoard();
+
+private:
+
+    uint16_t lastMove;
+
+    //Assuming white is on bottom
+    uint64_t whitePawns, whiteRooks, whiteKnights, whiteBishops, whiteQueens, whiteKings;
+    //Assuming black is on top
+    uint64_t blackPawns, blackRooks, blackKnights, blackBishops, blackQueens, blackKings;
 
 };
 
